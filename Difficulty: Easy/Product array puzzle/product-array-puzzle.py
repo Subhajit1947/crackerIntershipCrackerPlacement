ab4a656c35a8c1a7583c37f12699ab0d05ee21be
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2024-07-27 21:15:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+class Solution:
+    def productExceptSelf(self, nums, n):
+        res=[1]*len(nums)
+        prefix=1
+        for i in range(len(nums)):
+            res[i]=prefix
+            prefix*=nums[i]
+        postfix=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]*=postfix
+            postfix*=nums[i]
+        return res
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().split()]
+
+        ans=Solution().productExceptSelf(arr,n)
+        print(*ans)
+# } Driver Code Ends
