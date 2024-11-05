260a41606316f93d0341f4ecc8a0e6ff68712f7d
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2024-11-05 17:02:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.py
+++ Difficulty: Medium/Merge Sort/merge-sort.py
@@ -1,49 +1,54 @@
-#User function Template for python3
-
-class Solution:
-    def merge(self,arr, l, m, r): 
-        i=l
-        j=m+1
-        temp=[]
-        while i<=m and j<=r:
-            if arr[i]>=arr[j]:
-                temp.append(arr[j])
-                j+=1
-            else:
-                temp.append(arr[i])
-                i+=1
-        while i<=m:
-            temp.append(arr[i])
-            i+=1
-        while j<=r:
-            temp.append(arr[j])
-            j+=1
-        for k in range(l,r+1):
-            arr[k]=temp[k-l]
-    def mergeSort(self,arr, l, r):
-        if l<r:
-            mid=(l+r)//2
-            self.mergeSort(arr,l,mid)
-            self.mergeSort(arr,mid+1,r)
-            self.merge(arr,l,mid,r)
-            
-
-
-
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+# } Driver Code Ends
+#User function Template for python3
 
-import sys
-input = sys.stdin.readline
+class Solution:
+    
+    def merge(self,arr,l,m,r):
+        res=[]
+        i=l
+        j=m+1
+        while i<m+1 and j<r+1:
+            if arr[i]<=arr[j]:
+               res.append(arr[i])
+               i+=1
+            else:
+                res.append(arr[j])
+                j+=1
+        while i<m+1:
+            res.append(arr[i])
+            i+=1
+        while j<r+1:
+            res.append(arr[j])
+            j+=1
+        for ele in range(l,r+1):
+            arr[ele]=res[ele-l]
+        
+        
+    def mergeSort(self,arr, l, r):
+        if l<r:
+            m=(l+r)//2
+            self.mergeSort(arr,l,m)
+            self.mergeSort(arr,m+1,r)
+            self.merge(arr,l,m,r)
+
+
+#{ 
+ # Driver Code Starts.
+#Initial Template for Python 3
+
 if __name__ == "__main__":
-    t=int(input())
-    for i in range(t):
-        n=int(input())
-        arr=list(map(int,input().split()))
-        Solution().mergeSort(arr, 0, n-1)
-        for i in range(n):
-            print(arr[i],end=" ")
-        print()
+    t = int(input())
+    while t > 0:
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ob.mergeSort(arr,0,len(arr)-1)
+        print(*arr)
+        print("~")
+        t -= 1
+
+
 # } Driver Code Ends
