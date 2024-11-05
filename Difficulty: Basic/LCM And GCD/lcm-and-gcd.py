4404a973626cc88753e000df24df7805f965e313
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2024-11-05 19:12:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.py
@@ -1,15 +1,18 @@
 #User function Template for python3
 
 class Solution:
-    def lcmAndGcd(self, a, b):
-        n1,n2=a,b
-        while n1%n2!=0:
-            rem=n1%n2
-            n1=n2
-            n2=rem
-        gcd=n2
-        lcm=(a*b)//gcd
-        return lcm,gcd
+    def lcmAndGcd(self, a , b):
+        # code here 
+        if a==0 or b==0:
+            return 0
+        
+        a11,b11=a,b
+        while a%b!=0:
+            a,b=b%a,a
+        gcd=b
+        hcf=(a11*b11)//gcd
+        return hcf,gcd
+        
 
 
 #{ 
@@ -17,13 +20,15 @@ class Solution:
 #Initial Template for Python 3
 
 import math
-if __name__ == '__main__': 
-    t = int (input ())
-    for _ in range (t):
-        A,B=map(int,input().split())
-        
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        A, B = map(int, input().split())
+
         ob = Solution()
-        ptr = ob.lcmAndGcd(A,B)
-        
-        print(ptr[0],ptr[1])
+        ptr = ob.lcmAndGcd(A, B)
+
+        print(ptr[0], ptr[1])
+        print("~")
+
 # } Driver Code Ends
