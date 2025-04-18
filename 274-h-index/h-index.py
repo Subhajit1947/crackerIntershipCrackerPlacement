8eb8b326a0b53bc120b6c673ed6347e93658ed99
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2025-04-18 08:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (41.53%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        arr=[0]*(len(citations)+1)
+        for i in range(len(citations)):
+            if citations[i]>=len(citations):
+                arr[len(citations)]+=1
+            else:
+                arr[citations[i]]+=1
+        count=0      
+        for i in range(len(arr)-1,-1,-1):
+            if arr[i]+count>=i:
+                # print(i)
+                return i
+            count+=arr[i]
+        
