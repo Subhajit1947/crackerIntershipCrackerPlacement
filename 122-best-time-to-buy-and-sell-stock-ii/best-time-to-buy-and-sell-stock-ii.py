4ccb33c4e57f6d63e4f1a8e9b49d040816c92955
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2025-04-20 10:07:30 +0530

    Time: 42 ms (5.01%) | Memory: 27.8 MB (5.38%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -2,9 +2,8 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp={}
         def fn(idx,buy):
-            if idx==len(prices):
+            if idx>=len(prices):
                 return 0
-            profit=0
             if (idx,buy) in dp.keys():
                 return dp[(idx,buy)]
             else:
@@ -14,5 +13,4 @@ class Solution:
                     profit=max(prices[idx]+fn(idx+1,1),fn(idx+1,0))
                 dp[(idx,buy)]=profit
                 return profit
-
         return fn(0,1)
