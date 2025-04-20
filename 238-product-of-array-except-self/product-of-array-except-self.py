90ca3854948ebd09372d3f33484549d4b1052837
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2025-04-20 12:18:16 +0530

    Time: 27 ms (48.11%) | Memory: 26.4 MB (25.06%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,12 +3,11 @@ class Solution:
         pre=[1]
         suf=[1]
         ans=[]
-        for i in range(len(nums)-1):
-            pre.append(pre[-1]*nums[i])
-        for i in range(len(nums)-1,0,-1):
-            suf.append(suf[-1]*nums[i])
-        suf.reverse()
+        for i in nums[0:len(nums)-1]:
+            pre.append(pre[-1]*i)
+        for i in nums[len(nums)-1:0:-1]:
+            suf.append(suf[-1]*i)
+        suf=suf[::-1]
         for i in range(len(nums)):
             ans.append(pre[i]*suf[i])
         return ans
-            
