df1bf649d6441847e847e86b63ce0c5417d6639c
SUBHAJIT DAS <91960821+Subhajit1947@users.noreply.github.com>
2025-04-20 10:13:50 +0530

    Time: 3 ms (82.30%) | Memory: 19.4 MB (52.02%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         a=list(set(nums))
-        m=0
-        ele=None
+        ele=-1
+        c=0
         for i in a:
-            if m<nums.count(i):
-                m=nums.count(i)
+            if nums.count(i)>c:
                 ele=i
+                c=nums.count(i)
         return ele
